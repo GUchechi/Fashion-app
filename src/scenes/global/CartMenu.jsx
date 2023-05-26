bf7b0d0778a486977b9d0df4572478115311491c
GUchechi <godswilluchendu7@gmail.com>
2023-05-26 05:58:57 +0100

    totalPrice

--- src/scenes/global/CartMenu.jsx
+++ src/scenes/global/CartMenu.jsx
@@ -20,6 +20,15 @@ const FlexBox = styled(Box)`
 `;
 
 const CartMenu = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const cart = useSelector((state) => state.cart.cart);
+  const isCartOpen = useSelector((state) => state.cart.isCartOpen);
+
+  const totalPrice = cart.reduce((total, item) => {
+    return total + item.count * item.attributes.price;
+  }, 0);
+
   return <div>CartMenu</div>;
 };
 
