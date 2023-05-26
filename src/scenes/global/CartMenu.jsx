7a23f459da9dc9715aff92e18d9e72dc160cd32e
GUchechi <godswilluchendu7@gmail.com>
2023-05-26 05:43:34 +0100

    cartMenu

--- src/scenes/global/CartMenu.jsx
+++ src/scenes/global/CartMenu.jsx
@@ -1,4 +1,9 @@
-import React from 'react'
+import { Box, Button, Divider, IconButton, Typography } from "@mui/material";
+import { useSelector, useDispatch } from "react-redux";
+import CloseIcon from "@mui/icons-material/Close";
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
+import styled from "@emotion/styled";
 
 const CartMenu = () => {
   return (
