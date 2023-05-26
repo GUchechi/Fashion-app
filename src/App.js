24762ffdfc3aa3c9f4c7a9a17aefcf116f913000
GUchechi <godswilluchendu7@gmail.com>
2023-05-26 05:01:28 +0100

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
-import "./App.css";
 import { useEffect } from "react";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Home from "./scenes/home/Home";
 
 const ScrollToTop = () => {
