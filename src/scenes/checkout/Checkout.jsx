a3f6b84a5036b1980733816725c120b03bcb83f2
GUchechi <godswilluchendu7@gmail.com>
2023-05-28 20:28:05 +0100

    checkout

--- src/scenes/checkout/Checkout.jsx
+++ src/scenes/checkout/Checkout.jsx
@@ -4,15 +4,44 @@ import { Formik } from "formik";
 import { useState } from "react";
 import * as yup from "yup";
 import { shades } from "../../theme";
-import Payment from "./Payment";
-import Shipping from "./Shipping";
-import { loadStripe } from "@stripe/stripe-js";
+// import Payment from "./Payment";
+// import Shipping from "./Shipping";
+// import { loadStripe } from "@stripe/stripe-js";
 
 const Checkout = () => {
   const [activeStep, setActiveStep] = useState(0);
   const cart = useSelector((state) => state.cart.cart);
   const isFirstStep = activeStep === 0;
   const isSecondStep = activeStep === 1;
+
+  const handleFormSubmit = async (values, actions) => {setActiveStep(activeStep + 1);}
+
+
+const initialValues = {
+  billingAddress: {
+    firstName: "",
+    lastName: "",
+    country: "",
+    street1: "",
+    street2: "",
+    city: "",
+    state: "",
+    zipCode: "",
+  },
+  shippingAddress: {
+    isSameAddress: true,
+    firstName: "",
+    lastName: "",
+    country: "",
+    street1: "",
+    street2: "",
+    city: "",
+    state: "",
+    zipCode: "",
+  },
+  email: "",
+  phoneNumber: "",
+};
 
   return (
     <div>Checkout</div>
