0d5b7bab810889e1f1905b041605aae4daf1d89a
GUchechi <godswilluchendu7@gmail.com>
2023-05-29 08:37:41 +0100

    addressForm

--- src/scenes/checkout/AddressForm.jsx
+++ src/scenes/checkout/AddressForm.jsx
@@ -106,18 +106,7 @@ const AddressForm = ({
         helperText={formattedHelper("city")}
         sx={{ gridColumn: "span 2" }}
       />
-      <TextField
-        fullWidth
-        type="text"
-        label="State"
-        onBlur={handleBlur}
-        onChange={handleChange}
-        value={values.state}
-        name={formattedName("state")}
-        error={formattedError("state")}
-        helperText={formattedHelper("state")}
-        sx={{ gridColumn: "1fr" }}
-      />
+    
       <TextField
         fullWidth
         type="text"
