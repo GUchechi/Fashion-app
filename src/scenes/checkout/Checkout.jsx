78205460bd3e05780e34131d69fc2c92de6293a4
GUchechi <godswilluchendu7@gmail.com>
2023-05-30 10:09:15 +0100

    state

--- src/scenes/checkout/Checkout.jsx
+++ src/scenes/checkout/Checkout.jsx
@@ -47,16 +47,7 @@ const Checkout = () => {
       })),
     };
 
-    const response = await fetch("http://localhost:1337/api/orders", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(requestBody),
-    });
-    const session = await response.json();
-    await stripe.redirectToCheckout({
-      sessionId: session.id,
-    });
-  }
+    
 
   return (
     <Box width="80%" m="100px auto">
