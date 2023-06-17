967d707e644a28addbff18e8e3e2875e31cae391
GUchechi <godswilluchendu7@gmail.com>
2023-06-17 23:55:44 +0100

    refactoring

--- src/state/index.js
+++ src/state/index.js
@@ -30,7 +30,14 @@ export const cartSlice = createSlice({
         return item;
       });
     },
-  
+    decreaseCount: (state, action) => {
+      state.cart = state.cart.map((item) => {
+        if (item.id === action.payload.id && item.count > 1) {
+          item.count--;
+        }
+        return item;
+      });
+    },
   },
 });
 
